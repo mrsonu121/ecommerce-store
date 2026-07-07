12e545b37eed2e2261e8b155a06913d53eb0ca42
mrsonu121 <lodhisachin7734@gmail.com>
2026-07-07 10:04:38 +0530

    Prepare project for Render deployment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -3,18 +3,24 @@ Django settings for ecommerce project.
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-t7%*ikzvjc1u70sx-6s)qli1-j(u^2dnbn33sij*hiky_0^9&7'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-t7%*ikzvjc1u70sx-6s)qli1-j(u^2dnbn33sij*hiky_0^9&7"
+)
 
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
-# ==========================
-# Installed Apps
-# ==========================
+
+# ===================================================
+# INSTALLED APPS
+# ===================================================
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -34,37 +40,55 @@ INSTALLED_APPS = [
     'chat',
 ]
 
-# ==========================
-# Middleware
-# ==========================
+
+# ===================================================
+# MIDDLEWARE
+# ===================================================
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
+
     'django.middleware.common.CommonMiddleware',
+
     'django.middleware.csrf.CsrfViewMiddleware',
+
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+
     'django.contrib.messages.middleware.MessageMiddleware',
+
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
+
 
 ROOT_URLCONF = 'ecommerce.urls'
 
-# ==========================
-# Templates
-# ==========================
+
+# ===================================================
+# TEMPLATES
+# ===================================================
 
 TEMPLATES = [
+
     {
+
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
 
         'DIRS': [
+
             BASE_DIR / 'templates',
+
         ],
 
         'APP_DIRS': True,
 
         'OPTIONS': {
+
             'context_processors': [
 
                 'django.template.context_processors.request',
@@ -76,45 +100,68 @@ TEMPLATES = [
                 'context_processors.cart_count',
 
             ],
+
         },
+
     },
+
 ]
+
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
-# ==========================
-# Database
-# ==========================
+
+# ===================================================
+# DATABASE
+# ===================================================
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+    "default": dj_database_url.config(
+
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+
+    )
+
 }
 
-# ==========================
-# Password Validation
-# ==========================
+
+# ===================================================
+# PASSWORD VALIDATION
+# ===================================================
 
 AUTH_PASSWORD_VALIDATORS = [
+
     {
+
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+
     },
+
     {
+
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+
     },
+
     {
+
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+
     },
+
     {
+
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+
     },
+
 ]
 
-# ==========================
-# Internationalization
-# ==========================
+
+# ===================================================
+# INTERNATIONALIZATION
+# ===================================================
 
 LANGUAGE_CODE = 'en-us'
 
@@ -124,33 +171,43 @@ USE_I18N = True
 
 USE_TZ = True
 
-# ==========================
-# Static Files
-# ==========================
 
-STATIC_URL = 'static/'
+# ===================================================
+# STATIC FILES
+# ===================================================
+
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+
+    BASE_DIR / "static",
+
 ]
 
-# ==========================
-# Media Files
-# ==========================
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = '/media/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_ROOT = BASE_DIR / 'media'
 
-# ==========================
-# Default Auto Field
-# ==========================
+# ===================================================
+# MEDIA FILES
+# ===================================================
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+
+# ===================================================
+# DEFAULT AUTO FIELD
+# ===================================================
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# ==========================
-# Email Configuration
-# ==========================
+
+# ===================================================
+# EMAIL
+# ===================================================
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
@@ -160,8 +217,8 @@ EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "lodhisachin7734@gmail.com"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 
-EMAIL_HOST_PASSWORD = "qpjqnkjqrlkvtpds"
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
