f9f7b68c1c69ce2ca9afcff63be425e2b84c41ec
mrsonu121 <lodhisachin7734@gmail.com>
2026-07-11 10:19:26 +0530

    Fix create superuser

--- accounts/management/commands/create_superuser.py
+++ accounts/management/commands/create_superuser.py
@@ -4,52 +4,35 @@ import os
 
 
 class Command(BaseCommand):
-    help = "Create Superuser Automatically"
+    help = "Create or Update Superuser"
 
     def handle(self, *args, **kwargs):
 
         username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "admin12345")
 
-        email = os.environ.get(
-            "DJANGO_SUPERUSER_EMAIL",
-            "admin@example.com"
+        user, created = User.objects.get_or_create(
+            username=username,
+            defaults={
+                "email": email,
+                "is_staff": True,
+                "is_superuser": True,
+            }
         )
 
-        password = os.environ.get(
-            "DJANGO_SUPERUSER_PASSWORD",
-            "admin12345"
-        )
+        # Har deploy par password update hoga
+        user.email = email
+        user.is_staff = True
+        user.is_superuser = True
+        user.set_password(password)
+        user.save()
 
-        if not User.objects.filter(username=username).exists():
-
-            User.objects.create_superuser(
-
-                username=username,
-
-                email=email,
-
-                password=password
-
-            )
-
+        if created:
             self.stdout.write(
-
-                self.style.SUCCESS(
-
-                    "Superuser Created Successfully."
-
-                )
-
+                self.style.SUCCESS("Superuser Created Successfully.")
             )
-
         else:
-
             self.stdout.write(
-
-                self.style.WARNING(
-
-                    "Superuser Already Exists."
-
-                )
-
+                self.style.SUCCESS("Superuser Updated Successfully.")
             )
